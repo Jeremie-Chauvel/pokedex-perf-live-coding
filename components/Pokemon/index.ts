8dadaa2a01068c0011295ff7e9599b617544a938
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2023-01-26 18:28:51 +0100

    ✨ Create pokedex

---
+++ components/Pokemon/index.ts
@@ -0,0 +1,5 @@
+export { TypesList } from "./TypesList";
+export { CallMyPokemon } from "./CallMyPokemon";
+export { PokemonRow } from "./PokemonRow";
+export { Pokemon } from "./Pokemon";
+export { PokemonLazy } from "./PokemonLazy";
